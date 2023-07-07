01f13ebfa3ad6ba4ff2885171332fe7e6313be5b
DinhLeHoan <dinhlehoan21101@gmail.com>
2023-07-07 11:28:38 +0700

    changeToFireBase

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebase = require("firebase");
+const firebaseConfig = {
+    apiKey: "AIzaSyAydhNBsKkslRoGKvSj3-yI8GE2jo7FLJo",
+    authDomain: "textmind-ce96f.firebaseapp.com",
+    databaseURL: "https://textmind-ce96f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "textmind-ce96f",
+    storageBucket: "textmind-ce96f.appspot.com",
+    messagingSenderId: "518243305900",
+    appId: "1:518243305900:web:3297a21724473284215209",
+    measurementId: "G-NM74MXCR1E"
+  };
+firebase.initializeApp(firebaseConfig)
--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const PORT  = process.env.PORT || 3000;
+
+const firebase = require('firebase');
+const firebaseConfig = require('./TestSMS/config');
+const database = firebase.database();
+firebase.initializeApp(firebaseConfig);
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.write(`<h1>heo ne: ${PORT}</h1>`)
-  res.end
+  res.write(`<h1>heo ne: ${PORT}</h1>`);
+  res.end();
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on(`message`,(ms)=>{io.emit(`message`,ms)})
-});
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+// Read data from the database
+database
+  .ref('messages')
+  .once('value')
+  .then((snapshot) => {
+    const messages = snapshot.val();
+    console.log(messages);
+  })
+  .catch((error) => {
+    console.error('Error reading data:', error);
+  });
+
+// Write data to the database
+const newMessage = 'Hello, Firebase!';
+database
+  .ref('messages')
+  .push(newMessage)
+  .then(() => {
+    console.log('Data written successfully');
+  })
+  .catch((error) => {
+    console.error('Error writing data:', error);
+  });
